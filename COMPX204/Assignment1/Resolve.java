3f30ad6be7780db15e9306780cc45d1bf4616fe5
Troy West <troy.west@enlighten.co.nz>
2018-07-19 18:59:03 +1200

    Completed Resolve.java for COMPX204 Assignment 1

--- COMPX204/Assignment1/Resolve.java
+++ COMPX204/Assignment1/Resolve.java
@@ -1,3 +1,20 @@
+import java.io.*;
+import java.net.*;
+
 public class Resolve {
-    
+    public static void main(String args[]) {
+        if (args.length < 1) {
+            System.out.println("Usage: Resolve <name1> <name2> ... <nameN>");
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                String address = args[i];
+                try {
+                    InetAddress IP = InetAddress.getByName(address);
+                    System.out.println(address + " : " + IP.getHostAddress());
+                } catch (UnknownHostException e) {
+                    System.err.println(address + " : unknown host");
+                }
+            }
+        }
+    }
 }
