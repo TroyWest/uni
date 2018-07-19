39b6188e111c04d54243fd4adcd2d9976e1b194e
Troy West <troy.west@enlighten.co.nz>
2018-07-19 19:17:05 +1200

    Added SimpleServer.java for assignment 1

---
+++ COMPX204/Assignment1/SimpleServer.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.net.*;
+
+public class SimpleServer extends Thread {
+    @Override run() {
+
+    }
+
+    public static void main(String args[]) {
+        
+    }
+}
