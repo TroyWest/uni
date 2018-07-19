67f1ddf3fb09d1af2ff7d7a39f14801efe8e684c
Troy West <troy.west@enlighten.co.nz>
2018-07-19 20:02:50 +1200

    SimpleSever complete for assignment 1

--- COMPX204/Assignment1/SimpleServer.java
+++ COMPX204/Assignment1/SimpleServer.java
@@ -1,12 +1,30 @@
 import java.io.*;
 import java.net.*;
 
-public class SimpleServer extends Thread {
-    @Override run() {
-
-    }
-
+public class SimpleServer {
+    
     public static void main(String args[]) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234);
+            
+            while(true) {
+                System.out.println("Listening on port: " + serverSocket.getLocalPort());
+                Socket client = serverSocket.accept();
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                InetAddress clientAddress = client.getInetAddress();
+                System.out.println("Connection from: " + clientAddress.getHostName());
+                out.println("Hello, " + clientAddress.getHostName());
+                out.println("Your IP address is " + clientAddress.getHostAddress());
+                Thread.sleep(3000);
+                client.close();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        } catch (InterruptedException e) {
+
+        }
         
     }
 }
