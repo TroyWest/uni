f3bc0760d5cabd4ef8c286d88b948044e16b4641
Troy West <tald@xtra.co.nz>
2018-07-30 15:11:12 +1200

    Basic connection

--- COMPX204/Assignment2/HttpServer.java
+++ COMPX204/Assignment2/HttpServer.java
@@ -2,8 +2,28 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
+class HttpServerSession extends Thread {
+    private Socket httpServerSessionSocket;
+
+    HttpServerSession(Socket connectionSocket) {
+        httpServerSessionSocket = connectionSocket;
+        System.out.println("Connection on port " + connectionSocket.getPort());
+    }
+
+}
+
 class HttpServer {
     public static void main(String args[]) {
-        // write something to the console here
+        try {
+            // write something to the console here
+            ServerSocket serverSocket = new ServerSocket(8080);
+            // Socket client = serverSocket.accept();
+            while (true) {
+                HttpServerSession session = new HttpServerSession(serverSocket.accept());// client);
+            }
+        } catch (IOException e) {
+
+        }
+
     }
 }
