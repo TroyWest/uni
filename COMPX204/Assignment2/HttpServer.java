d43a394c29e19fef4933a0075f48fc0c3fdf1751
Troy West <tald@xtra.co.nz>
2018-07-30 14:35:29 +1200

    Created HttpServer file for assignment 2

---
+++ COMPX204/Assignment2/HttpServer.java
@@ -0,0 +1,9 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+class HttpServer {
+    public static void main(String args[]) {
+        // write something to the console here
+    }
+}
