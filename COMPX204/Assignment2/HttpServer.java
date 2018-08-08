e579b48db6cc19f44b02b64380e86db40b98dcf2
Troy West <tald@xtra.co.nz>
2018-08-08 22:09:06 +1200

    Initial server complete. No extension yet.

--- COMPX204/Assignment2/HttpServer.java
+++ COMPX204/Assignment2/HttpServer.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 class HttpServerSession extends Thread {
     private Socket httpServerSessionSocket;
+    private BufferedReader reader;
+    private BufferedOutputStream writer;
 
     HttpServerSession(Socket connectionSocket) {
         httpServerSessionSocket = connectionSocket;
@@ -12,8 +14,9 @@ class HttpServerSession extends Thread {
 
     public void run() {
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(httpServerSessionSocket.getInputStream()));
-
+            reader = new BufferedReader(new InputStreamReader(httpServerSessionSocket.getInputStream()));
+            writer = new BufferedOutputStream(httpServerSessionSocket.getOutputStream());
+            FileInputStream fin;
             String request = reader.readLine();
 
             String parts[] = request.split(" ");
@@ -22,14 +25,66 @@ class HttpServerSession extends Thread {
                 return;
             } else {
                 if (parts[0].compareTo("GET") == 0) {
-                    System.out.println(parts[1]);
+                    System.out.println(parts[1].substring(1));
+                    File page = new File(parts[1].substring(1));
+                    while (true) {
+                        String line = reader.readLine();
+                        if (line == null) {
+
+                        }
+                        if (line.compareTo("") == 0) {
+                            break;
+                        }
+                    }
+                    if (page.exists()) {
+                        fin = new FileInputStream(page);
+
+                        System.out.println("Sending Response");
+                        println(writer, "HTTP/1.1 200 OK");
+                        println(writer, "");
+                        
+                        SendFile( fin);
+                    } else {
+                        send404();
+                    }
                 }
                 httpServerSessionSocket.close();
             }
 
         } catch (IOException e) {
+            System.err.println("IO Error" + e.getMessage());
+        }
+    }
+
+    private void send404() throws IOException {
+        println(writer, "HTTP/1.1 404 File Not Found");
+        println(writer, "");
+        println(writer, "File not found");
+    }
+
+    private void SendFile(FileInputStream fin) throws IOException {
+        byte[] array = new byte[10240];
+        while (fin.read(array) != -1) {
+            writer.write(array);
+            writer.flush();
+            // try {
+            //     Thread.sleep(1000);
+            // } catch (InterruptedException e) {
+
+            // }
+        }
+
+    }
+
+    private void println(BufferedOutputStream bos, String s) throws IOException {
+        String news = s + "\r\n";
+        byte[] array = news.getBytes();
+        for (int i = 0; i < array.length; i++) {
+            bos.write(array[i]);
 
         }
+        bos.flush();
+        return;
     }
 
 }
@@ -38,12 +93,12 @@ class HttpServer {
     public static void main(String args[]) {
         try {
             // write something to the console here
-            ServerSocket serverSocket = new ServerSocket(8080);            
+            ServerSocket serverSocket = new ServerSocket(8080);
             while (true) {
                 HttpServerSession session = new HttpServerSession(serverSocket.accept());
                 session.start();
             }
-            serverSocket.close();
+            // serverSocket.close();
         } catch (IOException e) {
 
         }
