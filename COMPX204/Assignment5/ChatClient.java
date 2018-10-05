9f69fc69dd66a4a804a2a2e972cf9c627c12c2cb
Troy West <tald@xtra.co.nz>
2018-10-05 16:17:23 +1300

    chat client scaffolding

--- COMPX204/Assignment5/ChatClient.java
+++ COMPX204/Assignment5/ChatClient.java
@@ -1,3 +1,16 @@
+import java.lang.*;
+
+class ChatSession extends Thread {    
+    public void run() {
+        System.out.println("Running");
+    }
+
+}
+
 class ChatClient {
-    
+    public static void main(String[] args) {
+        ChatSession session = new ChatSession();
+        session.start();
+    }
+
 }
