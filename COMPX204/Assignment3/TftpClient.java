340b182cdd1db37effdfd9a3183f7f5817b5e220
Troy West <tald@xtra.co.nz>
2018-10-11 00:35:41 +1300

    Completed assignment 3

--- COMPX204/Assignment3/TftpClient.java
+++ COMPX204/Assignment3/TftpClient.java
@@ -8,24 +8,23 @@ public class TftpClient {
 
     public static byte[] unpackDataPacket(DatagramPacket p) {
         byte[] packetData = p.getData();
-        byte[] data = new byte[packetData.length - 2];
-        for (int i = 2; i < packetData.length; i++) {
-            data[i - 2] = packetData[i];
-        }
-        return data;
+        int length = p.getLength();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(length - 2); //ignore packet type and block number
+        byteBuffer.put(packetData, 2, length - 2);
+        return byteBuffer.array();
     }
 
-    public static byte[] createAckPacket(byte block){
+    public static byte[] createAckPacket(byte block) {
         byte[] ackPack = new byte[2];
-        ackPack[0] =  TftpUtility.ACK;
+        ackPack[0] = TftpUtility.ACK;
         ackPack[1] = block;
         return ackPack;
     }
 
     public static void main(String[] args) {
         if (args.length < 3) {
-            System.out.println("Nope!");
-            return;
+            System.out.println("Usage: TftpClient server_ip server_port filename");
+            System.exit(0);
         }
         try {
             InetAddress serverAddress = InetAddress.getByName(args[0]);
@@ -33,48 +32,50 @@ public class TftpClient {
             DatagramSocket socket = new DatagramSocket();
             String fileName = args[2];
             System.out.println("Connecting to " + args[0] + ":" + port);
-            
-            byte[] buf = new byte[1472];
+
+            byte[] buf = new byte[514];
 
             DatagramPacket p = TftpUtility.packRRQDatagramPacket(fileName.getBytes()); // new DatagramPacket(buf, 1472);
             p.setAddress(serverAddress);
             p.setPort(port);
             socket.send(p);
 
-            p = new DatagramPacket(buf, 1472);
+            FileOutputStream fos = new FileOutputStream("./out/" + fileName);
+            int off = 0;
 
             boolean reading = true;
+            System.out.println("Recieving file");
             while (reading) {
+                p = new DatagramPacket(buf, 514);
                 socket.receive(p);
 
                 Byte response = TftpUtility.checkPacketType(p);
+
                 if (response == TftpUtility.ERROR) {
                     TftpUtility.printErrorString(p);
-                } else {                    
+                } else {
                     byte blockNumber = TftpUtility.extractBlockSeq(p);
-                    if (blockNumber == -1) {
-                        System.out.println("Not a data packet");
-                        return;
-                    } else {
-                        byte[] data = unpackDataPacket(p);                        
-                        String s = new String(data);
-                        System.out.println(s);
-                        
-                        p.setData(createAckPacket(blockNumber));
-                        socket.send(p);
-                        if(p.getLength() < 512) {
-                            reading = false;
-                        }                        
+                    byte[] data = unpackDataPacket(p);                    
+                    fos.write(data);
+                    int length = p.getLength();
+                    p.setData(createAckPacket(blockNumber));
+                    System.out.print(".");                    
+                    socket.send(p);
+                    if (length < 512) {
+                        reading = false;
                     }
-
                 }
             }
-
+            System.out.println("\nTransfer complete");
+            fos.flush();
+            fos.close();
             socket.close();
 
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getMessage());
+        } finally {
+
         }
     }
 }
